cac7564ea9885f0f9958d6ee2d8cc785b02926ab
Oleg Polyakov <olegpolyako@outlook.com>
2017-12-07 20:10:04 +0300

    End

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import './App.css';
 import logo from './logo.svg';
@@ -6,18 +6,25 @@ import logo from './logo.svg';
 export default class App extends Component {
     render() {
         return (
-            <div className="app">
+            <Fragment>
                 <header>
                     <img src={logo} alt="React logo" />
                     <h1>React 16</h1>
                 </header>
 
                 <main>
-                    
+                    <dl>
+                        {[[1, 'a'], [2, 'b'], [3, 'c']].map(([dd, dt]) =>
+                            <Fragment key={dd}>
+                                <dd>{dd}</dd>
+                                <dt>{dt}</dt>
+                            </Fragment>
+                        )}
+                    </dl>
                 </main>
 
                 <footer></footer>
-            </div>
+            </Fragment>
         );
     }
 }
